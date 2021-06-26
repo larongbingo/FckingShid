e7d530c9f1de0d5ff3ea211bb8386afea8a4dee1
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-06-26 20:30:00 +0800

    mga egul, ako magiisip ng mga resorts, fck dat

--- src/MappingShidPenis.ts
+++ src/MappingShidPenis.ts
@@ -1,26 +1,30 @@
 /**
  * Written by Renz Christen Yeomer A. Pagulayan
  */
-
-// @ts-ignore
-import Mapbox from "mapbox-gl/dist/mapbox-gl-csp";
-import DirectionClient from "@mapbox/mapbox-sdk/services/directions";
-import { getLocation } from "./utils";
 // @ts-ignore 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
 
+
+import Mapbox from "mapbox-gl";
+import DirectionClient from "@mapbox/mapbox-sdk/services/directions";
+import { Coordinates } from "@mapbox/mapbox-sdk/lib/classes/mapi-request";
+import { getLocation } from "./utils";
+
+// @ts-ignore
 Mapbox.workerClass = MapboxWorker; 
 Mapbox.accessToken = 'pk.eyJ1IjoibGFyb25nYmluZ28iLCJhIjoiY2txZGU0ZXM5MGtyeTJ2bnppOHN3b3R2eSJ9.rrO7005dsxNQTb-Cl_8GAQ';
 let directionClient = DirectionClient({
   accessToken: Mapbox.accessToken,
 });
 
+const pansol: Coordinates = [121.1840, 14.1784];
+
 (async function () {
   const map = new Mapbox.Map({
     container: 'map-container', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [121.1840, 14.1784], // starting position [lng, lat]
+    center: pansol, // starting position [lng, lat]
     zoom: 15 // starting zoom
   });
 
@@ -32,25 +36,23 @@ let directionClient = DirectionClient({
     draggable: false,
   });
 
-
-  // get current location via GeoLocation APIs
-
+  // get current location via GeoLocation Browser APIs
   var coords: GeolocationPosition | null = null;
   if (navigator.geolocation) {
     var location = await getLocation();
     coords = location;
     console.log(location);
     currentPositionMarker.setLngLat([location.coords.longitude, location.coords.latitude]);
-    
     currentPositionMarker.addTo(map);
   }
 
+  // Pansol Marker
   new Mapbox.Marker({
     color: "#FFF",
     draggable: true,
   })
-    .setLngLat([121.1840, 14.1784])
-    .addTo(map);
+  .setLngLat(pansol)
+  .addTo(map);
 
   map.on("load", () => {
     directionClient.getDirections({
@@ -62,14 +64,15 @@ let directionClient = DirectionClient({
           coordinates: [coords!.coords.longitude, coords!.coords.latitude]
         },
         {
-          coordinates: [121.1840, 14.1784]
+          coordinates: pansol
         }
       ]
     })
     .send()
     .then(res => {
       console.log(res);
-  
+      
+      // Setup geojson shit to be able to add to the fcking map
       var geojson = {
         type: 'Feature',
         properties: {},
@@ -79,7 +82,8 @@ let directionClient = DirectionClient({
         }
       };
       console.log(res.body.routes[0].geometry.coordinates)
-  
+      
+      // Add the geometry to the fcking map so that they can choke on it
       map.addLayer({
         id: 'route',
         type: 'line',
@@ -100,11 +104,7 @@ let directionClient = DirectionClient({
         }
       })
     });
-  
   });
-
-  // Add the geometry to the fcking map so that they can choke on it
-
 })();
 
 /**
